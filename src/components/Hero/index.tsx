ff4e550e37515441b0420fbe20539dfb6e0842c7
kbventures <kmb_x@hotmail.com>
2023-06-28 18:08:19 -0500

    Feature section without links complete

---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,33 @@
+import { useTranslation } from "react-i18next";
+import LinkButton from "../LinkButton/index";
+
+import styles from "./hero.module.scss";
+
+const { hero, ContentWrapper, SubTitle, Content, Row } = styles;
+
+function Hero() {
+    const { t } = useTranslation("hero");
+
+    return (
+        <section className={hero}>
+            <div className={ContentWrapper}>
+                <h1 className={SubTitle}>{t("freedom")}</h1>
+                <p className={Content}>{t("ourAnalysis")}</p>
+                <div className={Row}>
+                    <LinkButton to="/signup" className="Primary">
+                        {t("signup")}
+                    </LinkButton>
+                    <LinkButton
+                        to="/demo"
+                        className="Secondary"
+                        otherClassName="margin"
+                    >
+                        {t("demo")}
+                    </LinkButton>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Hero;
