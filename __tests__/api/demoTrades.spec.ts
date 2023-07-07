111e21cb13b7152607b9485ba12d767560e21ac9
kbventures <kmb_x@hotmail.com>
2023-07-07 11:32:55 -0500

    Clean up and new api end up code

--- __tests__/api/demoTrades.spec.ts
+++ __tests__/api/demoTrades.spec.ts
@@ -1,67 +1,63 @@
-import httpMocks from 'node-mocks-http';
-import demoExchangeTrades from '../pages/api/demoExchangeTrades';
+import httpMocks from "node-mocks-http";
+import demoExchangeTrades from "../../src/pages/api/demoTrades";
 
-describe('/api/demoExchangeTrades', () => {
-  it('should return mock trade data', async () => {
-    const req = httpMocks.createRequest();
-    const res = httpMocks.createResponse();
+describe("/api/demoExchangeTrades", () => {
+    it("should return mock trade data", async () => {
+        const req = httpMocks.createRequest();
+        const res = httpMocks.createResponse();
 
-    await demoExchangeTrades(req, res);
+        await demoExchangeTrades(req, res);
 
-    // Check the response status code
-    expect(res.statusCode).toBe(200);
+        // Check the response status code
+        expect(res.statusCode).toBe(200);
 
-    // Parse the response body as JSON
-    const responseBody = JSON.parse(res._getData());
+        // Parse the response body as JSON
+        // eslint-disable-next-line no-underscore-dangle
+        const responseBody = JSON.parse(res._getData());
 
-    // Check the structure of the response
-    expect(responseBody).toHaveProperty('error', []);
-    expect(responseBody).toHaveProperty('result');
+        // Check the structure of the response
+        expect(responseBody).toHaveProperty("error", []);
+        expect(responseBody).toHaveProperty("result");
 
-    // Check the structure of the 'result' object
-    const { result } = responseBody;
-    expect(result).toHaveProperty('trades');
+        // Check the structure of the 'result' object
+        const { result } = responseBody;
+        expect(result).toHaveProperty("trades");
 
-    // Check individual trades
-    const trades = result.trades;
-    expect(trades).toHaveProperty('THVRQM-33VKH-UCI7BS');
-    expect(trades['THVRQM-33VKH-UCI7BS']).toEqual({
-      ordertxid: 'OQCLML-BW3P3-BUCMWZ',
-      postxid: 'TKH2SE-M7IF5-CFI7LT',
-      pair: 'XXBTZUSD',
-      time: 1688667796.8802,
-      type: 'buy',
-      ordertype: 'limit',
-      price: '30010.00000',
-      cost: '600.20000',
-      fee: '0.00000',
-      vol: '0.02000000',
-      margin: '0.00000',
-      misc: '',
+        // Check individual trades
+        const { trades } = result;
+        expect(trades).toHaveProperty("THVRQM-33VKH-UCI7BS");
+        expect(trades["THVRQM-33VKH-UCI7BS"]).toEqual({
+            ordertxid: "OQCLML-BW3P3-BUCMWZ",
+            postxid: "TKH2SE-M7IF5-CFI7LT",
+            pair: "XXBTZUSD",
+            time: 1688667796.8802,
+            type: "buy",
+            ordertype: "limit",
+            price: "30010.00000",
+            cost: "600.20000",
+            fee: "0.00000",
+            vol: "0.02000000",
+            margin: "0.00000",
+            misc: "",
+        });
+
+        expect(trades).toHaveProperty("TCWJEG-FL4SZ-3FKGH6");
+        expect(trades["TCWJEG-FL4SZ-3FKGH6"]).toEqual({
+            ordertxid: "OQCLML-BW3P3-BUCMWZ",
+            postxid: "TKH2SE-M7IF5-CFI7LT",
+            pair: "XXBTZUSD",
+            time: 1688667769.6396,
+            type: "buy",
+            ordertype: "limit",
+            price: "30010.00000",
+            cost: "300.10000",
+            fee: "0.00000",
+            vol: "0.01000000",
+            margin: "0.00000",
+            misc: "",
+        });
+
+        // Additional checks based on your specific requirements
     });
-
-    expect(trades).toHaveProperty('TCWJEG-FL4SZ-3FKGH6');
-    expect(trades['TCWJEG-FL4SZ-3FKGH6']).toEqual({
-      ordertxid: 'OQCLML-BW3P3-BUCMWZ',
-      postxid: 'TKH2SE-M7IF5-CFI7LT',
-      pair: 'XXBTZUSD',
-      time: 1688667769.6396,
-      type: 'buy',
-      ordertype: 'limit',
-      price: '30010.00000',
-      cost: '300.10000',
-      fee: '0.00000',
-      vol: '0.01000000',
-      margin: '0.00000',
-      misc: '',
-    });
-
-    // Additional checks based on your specific requirements
-  });
 });
 // In this extended example, each trade is individually checked for its properties, ensuring that the response structure and data match your expectations. Customize the assertions based on your API's expected behavior and the details you want to verify in the response.
-
-
-
-
-
--- src/pages/api/krakenApi.ts
+++ src/pages/api/krakenApi.ts
@@ -1,134 +1,245 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import crypto from "crypto";
-// eslint-disable-next-line import/no-extraneous-dependencies
-import qs from "qs";
+const axios = require("axios");
+const crypto = require('crypto');
+const WebSocket = require('ws');
+require('dotenv').config();
 
-interface KrakenApiRequest {
-    pair?: string;
-    type?: string;
-    ordertype?: string;
-    price?: number;
-    volume?: number;
-}
 
-const krakenApiUrl = "https://api.kraken.com";
-const apiKey = process.env.API_KEY || "";
-const apiSecret = process.env.API_SECRET || "";
+const Main = async () => {
 
-const getMessageSignature = (
-    path: string,
-    request: KrakenApiRequest,
-    secret: string,
-    nonce: number
-) => {
-    const message = qs.stringify(request);
-    const secretBuffer = Buffer.from(secret, "base64");
-    const hash = crypto.createHash("sha256");
-    const hmac = crypto.createHmac("sha512", secretBuffer);
-    const hashDigest = hash.update(nonce + message).digest("binary");
-    return hmac.update(path + hashDigest, "binary").digest("base64");
-};
-
-export const getTradeHistory = async (
-    req: NextApiRequest,
-    res: NextApiResponse
-) => {
+    // Load API keys from environment variables
+    const apiPublicKey = process.env.API_PUBLIC_KEY;
+    const apiPrivateKey = process.env.API_PRIVATE_KEY;
+    
     try {
-        const nonce = new Date().getTime() * 1000;
 
-        const request: KrakenApiRequest = {
-            // Define request parameters for trade history here
-        };
+        console.log("|=========================================|");
+        console.log("|      KRAKEN.COM NODEJS TEST APP         |");
+        console.log("|=========================================|");
+        console.log();
 
-        const path = "/0/private/TradesHistory";
+        /*
+        * PRIVATE REST API Examples
+        */
 
-        const signature = getMessageSignature(path, request, apiSecret, nonce);
+        if (1 == 1) {
+            let privateResponse = "";
+            let privateEndpoint = "ClosedOrders" 
+            let privateInputParameters = ""
 
-        const additionalRequestData = {
-            signature,
-            key: apiKey,
-            nonce,
-        };
 
-        const mergedRequest = { ...request, ...additionalRequestData };
-
-        const headers: Record<string, string> = {
-            "Content-Type": "application/x-www-form-urlencoded",
-        };
-
-        const response = await fetch(`${krakenApiUrl}${path}`, {
-            method: "POST",
-            headers,
-            body: qs.stringify(mergedRequest),
-        });
-
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
+            privateResponse = await QueryPrivateEndpoint(privateEndpoint, 
+                privateInputParameters,
+                apiPublicKey,
+                apiPrivateKey);
+            
+            console.log(privateResponse);
         }
 
-        const responseData = await response.json();
-        console.log("response data", responseData);
+        console.log("|=======================================|");
+        console.log("| END OF PROGRAM - HAVE A GOOD DAY :)   |");
+        console.log("|=======================================|");
+        console.log("\n");
 
-        res.status(200).json(responseData);
-    } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error("API Request Error:", error);
-        res.status(500).json({
-            error: "An error occurred while fetching trade history from Kraken.",
-        });
+    }
+    catch (e) {
+        console.log();
+        console.log("AN EXCEPTION OCCURED :(");
+        console.log(e);
+    }
+
+
+    /*
+    * Private REST API Endpoints
+    */
+
+    async function QueryPrivateEndpoint(endPointName, 
+                                        inputParameters, 
+                                        apiPublicKey, 
+                                        apiPrivateKey) {
+        const baseDomain = "https://api.kraken.com";
+        const privatePath = "/0/private/";
+        
+        const apiEndpointFullURL = baseDomain + privatePath + endPointName + "?" + inputParameters;
+        const nonce = Date.now().toString();
+        const apiPostBodyData = "nonce=" + nonce + "&" + inputParameters;
+       
+        const signature = CreateAuthenticationSignature(apiPrivateKey,
+                                                        privatePath,
+                                                        endPointName,
+                                                        nonce,
+                                                        apiPostBodyData);
+        
+        const httpOptions =
+        {
+            headers: { 'API-Key': apiPublicKey, 'API-Sign': signature }
+        };
+
+        console.log("Request URL:", apiEndpointFullURL);
+        console.log("Request Body:", apiPostBodyData);
+        console.log("Request Options:", httpOptions);
+        console.log("Request Options Headers:", httpOptions.headers);
+
+
+        let jsonData = await axios.post(apiEndpointFullURL, apiPostBodyData, httpOptions);
+
+        return jsonData.data.result;
+    }
+
+
+    /*
+    * Authentication Algorithm
+    */
+
+    function CreateAuthenticationSignature(apiPrivateKey, 
+                                            apiPath, 
+                                            endPointName, 
+                                            nonce,
+                                            apiPostBodyData){
+
+        const apiPost = nonce + apiPostBodyData;
+        const secret = Buffer.from(apiPrivateKey, 'base64');
+        const sha256 = crypto.createHash('sha256');
+        const hash256 = sha256.update(apiPost).digest('binary');
+        const hmac512 = crypto.createHmac('sha512', secret);
+        const signatureString = hmac512.update(apiPath + endPointName + hash256, 'binary').digest('base64');
+        return signatureString;
     }
 };
 
-export const getAccountBalances = async (
-    req: NextApiRequest,
-    res: NextApiResponse
-    // eslint-disable-next-line consistent-return
-) => {
-    try {
-        const nonce = new Date().getTime() * 1000;
 
-        const request: KrakenApiRequest = {
-            // Define request parameters for account balances here
-        };
+Main();
 
-        const path = "/0/private/QueryLedgers"; // Replace with the actual endpoint
+// import { NextApiRequest, NextApiResponse } from "next";
+// import crypto from "crypto";
+// // eslint-disable-next-line import/no-extraneous-dependencies
+// import qs from "qs";
 
-        const signature = getMessageSignature(path, request, apiSecret, nonce);
+// interface KrakenApiRequest {
+//     pair?: string;
+//     type?: string;
+//     ordertype?: string;
+//     price?: number;
+//     volume?: number;
+// }
 
-        const additionalRequestData = {
-            signature,
-            key: apiKey,
-            nonce,
-        };
+// const krakenApiUrl = "https://api.kraken.com";
+// const apiKey = process.env.API_KEY || "";
+// const apiSecret = process.env.API_SECRET || "";
 
-        const mergedRequest = { ...request, ...additionalRequestData };
+// const getMessageSignature = (
+//     path: string,
+//     request: KrakenApiRequest,
+//     secret: string,
+//     nonce: number
+// ) => {
+//     const message = qs.stringify(request);
+//     const secretBuffer = Buffer.from(secret, "base64");
+//     const hash = crypto.createHash("sha256");
+//     const hmac = crypto.createHmac("sha512", secretBuffer);
+//     const hashDigest = hash.update(nonce + message).digest("binary");
+//     return hmac.update(path + hashDigest, "binary").digest("base64");
+// };
 
-        const headers: Record<string, string> = {
-            "Content-Type": "application/x-www-form-urlencoded",
-        };
+// export const getTradeHistory = async (
+//     req: NextApiRequest,
+//     res: NextApiResponse
+// ) => {
+//     try {
+//         const nonce = new Date().getTime() * 1000;
 
-        const response = await fetch(`${krakenApiUrl}${path}`, {
-            method: "POST",
-            headers,
-            body: qs.stringify(mergedRequest),
-        });
+//         const request: KrakenApiRequest = {
+//             // Define request parameters for trade history here
+//         };
 
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
+//         const path = "/0/private/TradesHistory";
 
-        console.log("response", response);
-        // console.log("response result", response.result);
+//         const signature = getMessageSignature(path, request, apiSecret, nonce);
 
-        const responseData = await response.json();
-        console.log("responseData", responseData);
+//         const additionalRequestData = {
+//             signature,
+//             key: apiKey,
+//             nonce,
+//         };
 
-        return res.json(responseData);
-    } catch (error) {
-        // eslint-disable-next-line no-console
-        console.error("API Request Error:", error);
-        res.status(500).json({
-            error: "An error occurred while fetching account balances from Kraken.",
-        });
-    }
-};
+//         const mergedRequest = { ...request, ...additionalRequestData };
+
+//         const headers: Record<string, string> = {
+//             "Content-Type": "application/x-www-form-urlencoded",
+//         };
+
+//         const response = await fetch(`${krakenApiUrl}${path}`, {
+//             method: "POST",
+//             headers,
+//             body: qs.stringify(mergedRequest),
+//         });
+
+//         if (!response.ok) {
+//             throw new Error("Network response was not ok");
+//         }
+
+//         const responseData = await response.json();
+//         console.log("response data", responseData);
+
+//         res.status(200).json(responseData);
+//     } catch (error) {
+//         // eslint-disable-next-line no-console
+//         console.error("API Request Error:", error);
+//         res.status(500).json({
+//             error: "An error occurred while fetching trade history from Kraken.",
+//         });
+//     }
+// };
+
+// export const getAccountBalances = async (
+//     req: NextApiRequest,
+//     res: NextApiResponse
+//     // eslint-disable-next-line consistent-return
+// ) => {
+//     try {
+//         const nonce = new Date().getTime() * 1000;
+
+//         const request: KrakenApiRequest = {
+//             // Define request parameters for account balances here
+//         };
+
+//         const path = "/0/private/QueryLedgers"; // Replace with the actual endpoint
+
+//         const signature = getMessageSignature(path, request, apiSecret, nonce);
+
+//         const additionalRequestData = {
+//             signature,
+//             key: apiKey,
+//             nonce,
+//         };
+
+//         const mergedRequest = { ...request, ...additionalRequestData };
+
+//         const headers: Record<string, string> = {
+//             "Content-Type": "application/x-www-form-urlencoded",
+//         };
+
+//         const response = await fetch(`${krakenApiUrl}${path}`, {
+//             method: "POST",
+//             headers,
+//             body: qs.stringify(mergedRequest),
+//         });
+
+//         if (!response.ok) {
+//             throw new Error("Network response was not ok");
+//         }
+
+//         console.log("response", response);
+//         // console.log("response result", response.result);
+
+//         const responseData = await response.json();
+//         console.log("responseData", responseData);
+
+//         return res.json(responseData);
+//     } catch (error) {
+//         // eslint-disable-next-line no-console
+//         console.error("API Request Error:", error);
+//         res.status(500).json({
+//             error: "An error occurred while fetching account balances from Kraken.",
+//         });
+//     }
+// };
