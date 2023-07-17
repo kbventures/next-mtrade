b60b2406113aeb79ab370826d3de2d7e4dcdbbeb
kbventures <kmb_x@hotmail.com>
2023-07-17 16:01:27 -0600

    Installed and setup eslint and prettier

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,66 +1,80 @@
-import { PrismaAdapter } from '@next-auth/prisma-adapter';
-import { PrismaClient, User } from '@prisma/client';
-import { AuthOptions } from 'next-auth';
-import NextAuth from 'next-auth/next';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import { z } from 'zod';
-import bcrypt from 'bcryptjs';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient, User } from "@prisma/client";
+import { AuthOptions } from "next-auth";
+import NextAuth from "next-auth/next";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { z } from "zod";
+import bcrypt from "bcryptjs";
 
 const loginUserSchema = z.object({
-  username: z.string().regex(/^[a-z0-9_-]{3,15}$/g, 'Invalid username'),
-  password: z.string().min(5, 'Password should be minimum 5 characters'),
+    username: z.string().regex(/^[a-z0-9_-]{3,15}$/g, "Invalid username"),
+    password: z.string().min(5, "Password should be minimum 5 characters"),
 });
 const prisma = new PrismaClient();
 
 const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    CredentialsProvider({
-      credentials: {
-        username: { type: 'text', placeholder: 'test@test.com' },
-        password: { type: 'password', placeholder: 'Pa$$w0rd' },
-      },
-      async authorize(credentials, req) {
-        const { username, password } = loginUserSchema.parse(credentials);
-        const user = await prisma.user.findUnique({
-          where: { username },
-        });
-        if (!user) return null;
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        CredentialsProvider({
+            credentials: {
+                username: { type: "text", placeholder: "test@test.com" },
+                password: { type: "password", placeholder: "Pa$$w0rd" },
+            },
+            async authorize(credentials) {
+                const { username, password } =
+                    loginUserSchema.parse(credentials);
+                const user = await prisma.user.findUnique({
+                    where: { username },
+                });
+                if (!user) return null;
 
-        const isPasswordValid = await bcrypt.compare(password, user.password);
+                const isPasswordValid = await bcrypt.compare(
+                    password,
+                    user.password
+                );
 
-        if (!isPasswordValid) return null;
+                if (!isPasswordValid) return null;
 
-        return user;
-      },
-    }),
-  ],
-  callbacks: {
-    session({ session, token }) {
-      console.log("account ...nextauth",session)
-      console.log("token ...nextauth",token)
-      session.user.id = token.id;
-      session.user.username = token.username;
-      return session;
+                return user;
+            },
+        }),
+    ],
+    callbacks: {
+        session({ session, token }) {
+            // console.log("account ...nextauth", session);
+            // console.log("token ...nextauth", token);
+            // Create a new session object with the necessary modifications
+            return {
+                ...session,
+                user: {
+                    ...session.user,
+                    id: token.id,
+                    username: token.username,
+                },
+            };
+        },
+        jwt({ token, account, user }) {
+            // console.log("account ...nexta", account);
+            const modifiedToken = {
+                ...token,
+            };
+
+            if (account) {
+                modifiedToken.accessToken = account.access_token;
+                modifiedToken.id = user.id;
+                modifiedToken.username = (user as User).username;
+            }
+
+            return modifiedToken;
+        },
     },
-    jwt({ token, account, user }) {
-      console.log("account ...nexta",account)
-      if (account) {
-        token.accessToken = account.access_token;
-        token.id = user.id;
-        token.username = (user as User).username;
-        console.log("jwt user ...nextauth", { user });
-      }
-      return token;
+    pages: {
+        signIn: "/login",
     },
-  },
-  pages: {
-    signIn: '/login',
-  },
-  session: {
-    strategy: 'jwt',
-  },
-  secret: process.env.JWT_SECRET,
+    session: {
+        strategy: "jwt",
+    },
+    secret: process.env.JWT_SECRET,
 };
 
 export default NextAuth(authOptions);
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,31 +1,40 @@
-import { signIn } from 'next-auth/react';
-import Link from 'next/link';
-import { FormEvent } from 'react';
+import { signIn } from "next-auth/react";
+import Link from "next/link";
+import { FormEvent } from "react";
 
 export default function Login() {
-  async function handleSubmit(e: FormEvent) {
-    e.preventDefault();
-    const form = new FormData(e.target as HTMLFormElement);
+    async function handleSubmit(e: FormEvent) {
+        e.preventDefault();
+        const form = new FormData(e.target as HTMLFormElement);
 
-    await signIn('credentials', {
-      username: form.get('username'),
-      password: form.get('password'),
-      callbackUrl: '/',
-    });
-  }
-  return (
-    <div className='container'>
-      <form onSubmit={handleSubmit}>
-        <h2>Login</h2>
-        <label htmlFor='username'>Username:</label>
-        <input type='text' id='username' name='username' required />
-        <label htmlFor='password'>Password:</label>
-        <input type='password' id='password' name='password' required />
-        <button type='submit'>Submit</button>
-      </form>
-      <p>
-        Not registered yet? <Link href='/register'>Register here</Link>
-      </p>
-    </div>
-  );
+        await signIn("credentials", {
+            username: form.get("username"),
+            password: form.get("password"),
+            callbackUrl: "/",
+        });
+    }
+    return (
+        <div className="container">
+            <form onSubmit={handleSubmit}>
+                <h2>Login</h2>
+                <label htmlFor="username">
+                    Username:
+                    <input type="text" id="username" name="username" required />
+                </label>
+                <label htmlFor="password">
+                    Password:
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        required
+                    />
+                </label>
+                <button type="submit">Submit</button>
+            </form>
+            <p>
+                Not registered yet? <Link href="/register">Register here</Link>
+            </p>
+        </div>
+    );
 }
