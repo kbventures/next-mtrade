28631dddfc00565b354ddff4d992c850a126e2c6
kbventures <kmb_x@hotmail.com>
2023-09-22 13:36:17 -0500

    Sign up form api call

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import { useState } from "react";
+import { useState, FormEvent } from "react";
 import { useTranslation } from "next-i18next";
+import { signIn } from "next-auth/react";
 import Link from "next/link";
 import Logo from "@/components/Logo/index";
 import LanguageSelector from "@/components/LanguageSelector";
@@ -29,18 +30,35 @@ const {
 
 function Login() {
     const { t } = useTranslation("login");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [username_, setUserName] = useState("");
+    const [password_, setPassword] = useState("");
     // const { login, error, isLoading } = useLogin();
     // const isLoading = true;
 
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore comment
-    const handleSubmit = async e => {
+    // eslint-disable-next-line consistent-return
+    async function handleSubmit(e: FormEvent) {
         e.preventDefault();
-
-        // await login(email, password);
-    };
+        // const form = new FormData(e.target as HTMLFormElement);
+        const res = await fetch("/api/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username: username_,
+                password: password_,
+            }),
+        });
+        const data = await res.json();
+        if (!data.user) return null;
+        await signIn("credentials", {
+            username: data.user.username,
+            password: password_,
+            callbackUrl: "/",
+        });
+    }
 
     return (
         <div className={authLayout}>
@@ -66,27 +84,27 @@ function Login() {
                 <form className={loginForm} onSubmit={handleSubmit}>
                     <h1 className={title}>{t("login")}</h1>
                     <p className={warning}>{t("visitingSiteWarning")}</p>
-                    <label className={label}>
+                    <label htmlFor="username" className={label}>
                         {t("username")}
                         <div className={cursor}>
                             <input
                                 className={loginInput}
                                 type="text"
-                                onChange={e => setEmail(e.target.value)}
-                                value={email}
+                                onChange={e => setUserName(e.target.value)}
+                                value={username_}
                                 placeholder={t("userNamePlaceHolder")}
                             />
                             <i />
                         </div>
                     </label>
-                    <label className={label}>
+                    <label htmlFor="password" className={label}>
                         {t("password")}
                         <div className={cursor}>
                             <input
                                 className={loginInput}
                                 type="password"
                                 onChange={e => setPassword(e.target.value)}
-                                value={password}
+                                value={password_}
                                 placeholder={t("password")}
                             />
                             <i />
@@ -121,35 +139,3 @@ export async function getStaticProps({ locale }: { locale: string }) {
 }
 
 export default Login;
-
-// import { signIn } from 'next-auth/react';
-// import Link from 'next/link';
-// import { FormEvent } from 'react';
-
-// export default function Login() {
-//   async function handleSubmit(e: FormEvent) {
-//     e.preventDefault();
-//     const form = new FormData(e.target as HTMLFormElement);
-
-//     await signIn('credentials', {
-//       username: form.get('username'),
-//       password: form.get('password'),
-//       callbackUrl: '/',
-//     });
-//   }
-//   return (
-//     <div className='container'>
-//       <form onSubmit={handleSubmit}>
-//         <h2>Login</h2>
-//         <label htmlFor='username'>Username:</label>
-//         <input type='text' id='username' name='username' required />
-//         <label htmlFor='password'>Password:</label>
-//         <input type='password' id='password' name='password' required />
-//         <button type='submit'>Submit</button>
-//       </form>
-//       <p>
-//         Not registered yet? <Link href='/register'>Register here</Link>
-//       </p>
-//     </div>
-//   );
-// }
