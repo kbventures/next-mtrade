d3bf7bff75a1eea0be30850188cda80998ecf00b
kbventures <kmb_x@hotmail.com>
2023-11-16 11:37:06 -0500

    Basic Test For Closed Trades

--- __tests__/api/krakenClosedTrades.spec.ts
+++ __tests__/api/krakenClosedTrades.spec.ts
@@ -5,12 +5,51 @@ import handler from "../../src/pages/api/krakenClosedTrades";
 
 jest.mock("axios");
 
+// Define an interface for the response object
+interface KrakenClosedTradeResponse {
+    message: string;
+    privateResponse: {
+        closed: {
+            [tradeId: string]: {
+                refid: string | null;
+                userref: string | null;
+                status: string;
+                opentm: number;
+                starttm: number;
+                expiretm: number;
+                descr: {
+                    pair: string;
+                    type: string;
+                    ordertype: string;
+                    price: string;
+                    price2: string;
+                    leverage: string;
+                    order: string;
+                    close: string;
+                };
+                vol: string;
+                vol_exec: string;
+                cost: string;
+                fee: string;
+                price: string;
+                stopprice: string;
+                limitprice: string;
+                misc: string;
+                oflags: string;
+                reason: string | null;
+                closetm: number;
+            };
+        };
+        count: number;
+    };
+}
+
 describe("API Tests", () => {
     const mockedResponse = {
         message: "Success",
         privateResponse: {
             closed: {
-                "OCOIRM-3AVHM-7B2YLW": {}, // You may need to adjust the expected structure here
+                "OCOIRM-3AVHM-7B2YLW": {},
             },
             count: 1,
         },
@@ -35,13 +74,101 @@ describe("API Tests", () => {
 
         // eslint-disable-next-line no-underscore-dangle
         const data = JSON.parse(mockResponse._getData());
-        // eslint-disable-next-line no-console
-        console.log("Received data:", data);
-        // eslint-disable-next-line no-console
-        // eslint-disable-next-line no-underscore-dangle
         expect(data.privateResponse.message).toBe("Success");
         expect(data.privateResponse.privateResponse.closed).toBeDefined();
+        expect(data.privateResponse.privateResponse.count).toBeDefined();
+        expect(data.privateResponse.privateResponse.count).toBe(1);
     });
 
-    // Add more test cases for error scenarios, missing environment variables, etc.
+    it("should handle details of a single closed trade in privateResponse", async () => {
+        const singleClosedTradeResponse: KrakenClosedTradeResponse = {
+            message: "Success",
+            privateResponse: {
+                closed: {
+                    "OCOIRM-3AVHM-7B2YLW": {
+                        refid: null,
+                        userref: null,
+                        status: "closed",
+                        opentm: 1588830213.657734,
+                        starttm: 0,
+                        expiretm: 0,
+                        descr: {
+                            pair: "BTCUSD",
+                            type: "buy",
+                            ordertype: "limit",
+                            price: "13226.9",
+                            price2: "0",
+                            leverage: "none",
+                            order: "buy 0.34 BTCUSD @ limit 13226.9",
+                            close: "",
+                        },
+                        // eslint-disable-next-line sonarjs/no-duplicate-string
+                        vol: "0.34000000",
+                        vol_exec: "0.34000000",
+                        cost: "4497.14600",
+                        fee: "11.69258",
+                        price: "13226.9",
+                        stopprice: "0.00000",
+                        limitprice: "0.00000",
+                        misc: "",
+                        oflags: "fciq",
+                        reason: null,
+                        closetm: 1588830213.6665378,
+                    },
+                },
+                count: 1,
+            },
+        };
+
+        (
+            axios.post as jest.MockedFunction<typeof axios.post>
+        ).mockResolvedValue({
+            data: { result: singleClosedTradeResponse },
+        } as AxiosResponse);
+
+        const mockRequest = httpMocks.createRequest({
+            method: "POST",
+            url: "/api/krakenClosedTrades",
+        });
+
+        const mockResponse = httpMocks.createResponse();
+
+        await handler(mockRequest, mockResponse);
+
+        // eslint-disable-next-line no-underscore-dangle
+        const data = JSON.parse(mockResponse._getData());
+        expect(data.privateResponse.message).toBe("Success");
+        expect(data.privateResponse.privateResponse.closed).toBeDefined();
+        expect(
+            data.privateResponse.privateResponse.closed["OCOIRM-3AVHM-7B2YLW"]
+        ).toBeDefined();
+        // Check specific properties of the closed trade
+        const tradeDetails =
+            data.privateResponse.privateResponse.closed["OCOIRM-3AVHM-7B2YLW"];
+        expect(tradeDetails.status).toBe("closed");
+        expect(tradeDetails.opentm).toBe(1588830213.657734);
+        expect(tradeDetails.starttm).toBe(0);
+        expect(tradeDetails.expiretm).toBe(0);
+        expect(tradeDetails.descr.pair).toBe("BTCUSD");
+        expect(tradeDetails.descr.type).toBe("buy");
+        expect(tradeDetails.descr.ordertype).toBe("limit");
+        expect(tradeDetails.descr.price).toBe("13226.9");
+        expect(tradeDetails.descr.price2).toBe("0");
+        expect(tradeDetails.descr.leverage).toBe("none");
+        expect(tradeDetails.descr.order).toBe(
+            "buy 0.34 BTCUSD @ limit 13226.9"
+        );
+        expect(tradeDetails.descr.close).toBe("");
+        expect(tradeDetails.vol).toBe("0.34000000");
+        expect(tradeDetails.vol_exec).toBe("0.34000000");
+        expect(tradeDetails.cost).toBe("4497.14600");
+        expect(tradeDetails.fee).toBe("11.69258");
+        expect(tradeDetails.price).toBe("13226.9");
+        expect(tradeDetails.stopprice).toBe("0.00000");
+        expect(tradeDetails.limitprice).toBe("0.00000");
+        expect(tradeDetails.misc).toBe("");
+        expect(tradeDetails.oflags).toBe("fciq");
+        expect(tradeDetails.reason).toBeNull();
+        expect(tradeDetails.closetm).toBe(1588830213.6665378);
+    });
 });
