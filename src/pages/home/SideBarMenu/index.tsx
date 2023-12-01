334343033d56262cadb5012fa24c7e6602bfdc33
kbventures <kmb_x@hotmail.com>
2023-12-01 14:15:58 -0500

    Home Link

--- src/pages/home/SideBarMenu/index.tsx
+++ src/pages/home/SideBarMenu/index.tsx
@@ -37,7 +37,7 @@ export default function index() {
                 <div className={accountSettingsSideNavWrapper}>
                     <div className={accountSettingsSideNavList}>
                         <Link
-                            href="/"
+                            href="/home"
                             className={`${accountSettingsSideNavListItem} ${active}`}
                         >
                             <FontAwesomeIcon
