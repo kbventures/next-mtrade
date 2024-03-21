f96af0a9ce0f58aab34c1cea8c048f7492ee9ae1
kbventures <kmb_x@hotmail.com>
2024-03-21 15:12:44 -0500

    Api-keys API refactored error handling

--- src/pages/api/api-keys.ts
+++ src/pages/api/api-keys.ts
@@ -11,13 +11,21 @@ export default async function handler(
         try {
             const apikeys = await prisma.apiKey.findMany();
             // Extracting only id and exchangeId from each object
-            const simplifiedApiKeys = apikeys.map(({ id, exchangeId }) => ({ id, exchangeId }));
+            const simplifiedApiKeys = apikeys.map(({ id, exchangeId }) => ({
+                id,
+                exchangeId,
+            }));
             res.status(200).json(simplifiedApiKeys);
         } catch (error) {
-            console.error("Error fetching Api Keys List:", error);
-            res.status(500).json({ error: "Internal Server Error" });
+            if (error instanceof Error) {
+                throw new Error(
+                    `Error fetching Api Keys List: ${error.message}`
+                );
+            } else {
+                throw new Error("Unknown error occurred");
+            }
         }
     } else {
-        res.status(405).json({ error: "Method Not Allowed" });
+        throw new Error("Method Not Allowed");
     }
 }
