8b0035f7eda8011c8e994aa2c505a6c52c12e32f
kbventures <kmb_x@hotmail.com>
2024-03-27 14:52:34 -0500

    Trades API Draft Update

--- src/pages/api/trades.ts
+++ src/pages/api/trades.ts
@@ -13,6 +13,7 @@ export default async function handler(
 ) {
     try {
         const apiKeys = await prisma.apiKey.findMany();
+        console.log("Trades Api apiKeys from db", apiKeys);
 
         // Initialize an object to store trades with associated API key information
         const tradesByExchangeAndApiKey: Record<string, { trades: unknown[] }> =
@@ -20,42 +21,56 @@ export default async function handler(
 
         // eslint-disable-next-line no-restricted-syntax
         for (const { publicKey, secretKey, exchangeId } of apiKeys) {
+            // console.log(
+            //     "Destructing apiKeys for loops",
+            //     publicKey,
+            //     secretKey,
+            //     exchangeId
+            // );
+
             let exchangeName = "";
             let apiKeyInfo = {};
 
-            if (exchangeId === "clu0coux50007p90ubwrc094p") {
+            if (exchangeId === "dcbb440d-4cc0-48a1-86ee-da6423c10b82") {
                 exchangeName = "commex";
-                apiKeyInfo = { exchangeId, publicKey, secretKey };
-            } else if (exchangeId === "clu8pug1z0001p90uik577s1l") {
+                apiKeyInfo = { exchangeId, publicKey, secretKey, exchangeName };
+            } else if (exchangeId === "10e44c1c-0395-4c63-90ae-40d2e1f6f4f8") {
                 exchangeName = "kraken";
-                apiKeyInfo = { exchangeId, publicKey, secretKey };
+                apiKeyInfo = { exchangeId, publicKey, secretKey, exchangeName };
             }
 
             if (exchangeName !== "") {
-                const exchange = new ccxt[exchangeName]({
+                const exchange = new ccxt[apiKeyInfo.exchangeName]({
                     apiKey: apiKeyInfo.publicKey,
                     secret: apiKeyInfo.secretKey,
                 });
 
+                console.log("Exhchange", exchange);
+
                 // eslint-disable-next-line no-await-in-loop
-                const trades = await exchange.fetchClosedOrders("DOT/USDT");
+                const { trades } = await exchange.fetchClosedOrders("DOT/USDT");
+
+                // eslint-disable-next-line no-console
+                console.log("Trades is working?", trades);
 
                 // Store trades along with API key information
-                tradesByExchangeAndApiKey[exchangeId] = {
-                    exchangeId: apiKeys.exchangeId,
+                tradesByExchangeAndApiKey[apiKeyInfo.exchangeName] = {
+                    exchange,
                     trades,
                 };
+                console.log(
+                    "tradesByExchangeApiKey",
+                    tradesByExchangeAndApiKey
+                );
             }
         }
 
-        res.status(200).json(tradesByExchangeAndApiKey);
+        // res.status(200).json(tradesByExchangeAndApiKey);
     } catch (error) {
-        throw new Error(
-            `Error fetching API keys: ${
-                error instanceof Error
-                    ? error.message
-                    : "Unknown error occurred"
-            }`
-        );
+        if (error instanceof Error) {
+            throw new Error(`Error fetching API keys: ${error.message}`);
+        } else {
+            throw new Error(`Error fetching API keys: Unknown error occurred`);
+        }
     }
 }
