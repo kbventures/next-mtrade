fb5542c0e4ba74cd9c7a3e4c1954d232203bf856
kbventures <kmb_x@hotmail.com>
2024-04-03 22:18:19 -0500

    trades api

--- src/pages/api/trades.ts
+++ src/pages/api/trades.ts
@@ -17,14 +17,34 @@ export default async function handler(
         const tradesByExchangeAndApiKey: Record<string, { trades: unknown[] }> =
             {};
 
+        // console.log("apiKeys", apiKeys);
+        // | apiKeys [
+        //     mtrade-app    |   {
+        //     mtrade-app    |     id: 'cluk19ack0001lh3wb9wag1br',
+        //     mtrade-app    |     keyAlias: 'Kraken Testing',
+        //     mtrade-app    |     publicKey: '8kAC/5horFHKdLin4eiCb+t8Dr8E4oCEwM97+M86t8DZaavJ3ArJVbr4',
+        //     mtrade-app    |     secretKey: 'zvKaDJsNgxiOx1gE/usnfk2/iYd17CFT+yX3GwpiV4HlkaAqdrPaGO723zDZCwqUaME/c0skb8kqjp+8WMgXAQ==',
+        //     mtrade-app    |     exchangeId: '8ef70434-92f0-41dd-8df6-3e520129c34c',
+        //     mtrade-app    |     userId: 'cluiwp8xr0000o33wratdt5yd'
+        //     mtrade-app    |   },
+        //     mtrade-app    |   {
+        //     mtrade-app    |     id: 'cluk1ecji0003lh3wn2qhrza8',
+        //     mtrade-app    |     keyAlias: 'Commex Testing',
+        //     mtrade-app    |     publicKey: '5A4D1D25AE95EB502061C061CAB3E3AB50A76CAC7D89FA3A4EB0B1E1157C1415',
+        //     mtrade-app    |     secretKey: 'zvKaDJsNgxiOx1gE/usnfk2/iYd17CFT+yX3GwpiV4HlkaAqdrPaGO723zDZCwqUaME/c0skb8kqjp+8WMgXAQ==',
+        //     mtrade-app    |     exchangeId: 'f8797c61-acf3-4157-9332-8e0eea186541',
+        //     mtrade-app    |     userId: 'cluiwp8xr0000o33wratdt5yd'
+        //     mtrade-app    |   }
+        //     mtrade-app    | ]
+
         // eslint-disable-next-line no-restricted-syntax
         for (const { publicKey, secretKey, exchangeId } of apiKeys) {
             let exchangeName = "";
             let apiKeyInfo = {};
 
             if (
-                exchangeId === "dcbb440d-4cc0-48a1-86ee-da6423c10b82" ||
-                exchangeId === "10e44c1c-0395-4c63-90ae-40d2e1f6f4f8"
+                exchangeId === "8ef70434-92f0-41dd-8df6-3e520129c34c"
+                // exchangeId === "f8797c61-acf3-4157-9332-8e0eea186541"
             ) {
                 exchangeName = "kraken";
                 apiKeyInfo = { exchangeId, publicKey, secretKey, exchangeName };
@@ -32,12 +52,26 @@ export default async function handler(
             // Need to be refactored to name instead of exchange id
 
             if (exchangeName !== "") {
-                const exchange = new ccxt[apiKeyInfo.exchangeName]({
-                    apiKey: apiKeyInfo.publicKey,
-                    secret: apiKeyInfo.secretKey,
+                const tempApiKey = apiKeyInfo.publicKey;
+                const tempSecret = apiKeyInfo.secretKey;
+                // eslint-disable-next-line new-cap
+                const kraken = new ccxt[exchangeName]({
+                    apiKey: tempApiKey,
+                    secret: tempSecret,
                 });
+
+                // const kraken = new ccxt.kraken({
+                //     apiKey: "8kAC/5horFHKdLin4eiCb+t8Dr8E4oCEwM97+M86t8DZaavJ3ArJVbr4",
+                //     secret: "zvKaDJsNgxiOx1gE/usnfk2/iYd17CFT+yX3GwpiV4HlkaAqdrPaGO723zDZCwqUaME/c0skb8kqjp+8WMgXAQ==",
+                // });
+                // This Works
+                // const kraken = new ccxt.kraken({
+                //     apiKey: "8kAC/5horFHKdLin4eiCb+t8Dr8E4oCEwM97+M86t8DZaavJ3ArJVbr4",
+                //     secret: "zvKaDJsNgxiOx1gE/usnfk2/iYd17CFT+yX3GwpiV4HlkaAqdrPaGO723zDZCwqUaME/c0skb8kqjp+8WMgXAQ==",
+                // });
                 // eslint-disable-next-line no-await-in-loop
-                const trades = await exchange.fetchClosedOrders();
+                const trades = await kraken.fetchClosedOrders();
+                // console.log(trades);
                 // eslint-disable-next-line no-await-in-loop
                 // console.log(await exchange.fetchClosedOrders());
 
@@ -45,6 +79,8 @@ export default async function handler(
                 // console.log("Trades is working?", trades);
 
                 // Store trades along with API key information
+                // console.log("Hello world!")
+                // console.log("trades", trades);
                 tradesByExchangeAndApiKey[apiKeyInfo.exchangeName] = {
                     trades,
                 };
@@ -56,7 +92,8 @@ export default async function handler(
         }
 
         // res.status(200).json(tradesByExchangeAndApiKey);
-        console.dir("har har", tradesByExchangeAndApiKey, { depth: null });
+        // console.dir("har har", tradesByExchangeAndApiKey, { depth: null });
+        // res.status(200).json(tradesByExchangeAndApiKey);
         res.status(200).json(tradesByExchangeAndApiKey);
     } catch (error) {
         if (error instanceof Error) {
@@ -66,3 +103,13 @@ export default async function handler(
         }
     }
 }
+
+// // upon instantiation
+// let commex = new ccxt.commex ({
+//     apiKey: '5A4D1D25AE95EB502061C061CAB3E3AB50A76CAC7D89FA3A4EB0B1E1157C1415',
+//     secret: 'D598291CF01C76F70A73FAAF52B18B184AAFDDAD4F80356C0CB30162FCE5841A',
+//   })
+//   let kraken = new ccxt.kraken ({
+//     apiKey: '8kAC/5horFHKdLin4eiCb+t8Dr8E4oCEwM97+M86t8DZaavJ3ArJVbr4',
+//     secret: 'zvKaDJsNgxiOx1gE/usnfk2/iYd17CFT+yX3GwpiV4HlkaAqdrPaGO723zDZCwqUaME/c0skb8kqjp+8WMgXAQ==',
+//   })
